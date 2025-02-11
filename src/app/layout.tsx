148cd2032c40f993e9e47c4c7574ecff6828f039
AasaSingh05 <aasasingh2005@gmail.com>
2025-02-11 16:33:04 +0530

    feat:Added description to the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,24 @@ import './globals.css'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Movie Recommender',
-  description: 'Get personalized movie recommendations',
+  title: 'CineMagic - AI-Powered Movie Recommendations 🎬✨',
+  description: 'Discover your next favorite movie with CineMagic! Our AI-powered system uses Machine Learning & NLP to provide personalized recommendations based on your taste.',
   icons: {
     icon: '/favicon.ico',
+  },
+  openGraph: {
+    title: 'CineMagic - AI-Powered Movie Recommendations 🎬✨',
+    description: 'Find movies you’ll love with our AI-driven recommendation engine! Powered by Machine Learning and NLP for smarter suggestions.',
+    url: 'https://cine-magic-five.vercel.app',
+    type: 'website',
+    images: [
+      {
+        url: 'https://cine-magic-five.vercel.app/preview-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'CineMagic Preview Image',
+      },
+    ],
   },
 }
 
